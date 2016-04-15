5850cc4669734d143fb545990106dfce0c3eeaa8
Steven Burrichter <steveb@stevencburrichter.com>
2016-04-15 12:39:50 -0700

    Added a quick shell script to test more quickly.

---
+++ src/shBlendShapeTester.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+python fbxBlendshapeConverter.py ~/OneDrive/_DEVELOPMENT/Houdini/FX_PACK/FX_BlendShapes/exports/MESH_SphereBlend.fbx ~/OneDrive/_DEVELOPMENT/Houdini/FX_PACK/FX_BlendShapes/exports/MESH_SphereBlend_Result.fbx
